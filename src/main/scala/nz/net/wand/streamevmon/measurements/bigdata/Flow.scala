750d83e16b270b294cf1055b6f36422215fd1221
Daniel Oosterwijk <doosterw@waikato.ac.nz>
2020-01-06 13:18:01 +1300

    Parse all currently measured Flows.

--- src/main/scala/nz/net/wand/streamevmon/measurements/bigdata/Flow.scala
+++ src/main/scala/nz/net/wand/streamevmon/measurements/bigdata/Flow.scala
@@ -8,52 +8,50 @@ import java.time.Instant
 import org.squeryl.annotations.Column
 
 case class Flow(
-  capture_application         : String,
+  capture_application: String,
   capture_host                : String,
 
   @Column("flow_id")
-  flow_id                     : Int,
+  stream: Int,
   @Column("type")
-  flow_type                   : String, // make enum
-  category                    : String, // maybe make enum
-  protocol                    : String, // maybe make enum
+  flow_type: String, // make enum
+  category: String, // maybe make enum
+  protocol: String, // maybe make enum
 
-  time                        : Instant, // time of current update
+  time: Instant, // time of current update
   @Column("start_ts")
-  start_time                  : Instant, // time of flow start
+  start_time: Instant, // time of flow start
   @Column("end_ts")
-  end_time                    : Option[Instant], // time of flow end,
+  end_time: Option[Instant], // time of flow end,
   duration                    : Double, // I have no idea what this is
 
   in_bytes                    : Int,
-  out_bytes                   : Int,
+  out_bytes: Int,
   @Column("ttfb")
   time_to_first_byte          : Double,
 
   // this should probably all be a case class by itself
-  destination_ip              : InetAddress,
+  destination_ip: InetAddress,
   @Column("dst_port")
   destination_port            : Int,
-  destination_ip_city         : String,
-  destination_ip_country      : String,
-  destination_ip_geohash      : String,
-  destination_ip_geohash_value: String,
-  destination_ip_latitude     : String,
-  destination_ip_longitude    : String,
+  destination_ip_city         : Option[String],
+  destination_ip_country      : Option[String],
+  destination_ip_geohash      : Option[String],
+  destination_ip_geohash_value: Option[Int],
+  destination_ip_latitude     : Option[Double],
+  destination_ip_longitude    : Option[Double],
 
-  source_ip                   : InetAddress,
-  @Column("dst_port")
+  source_ip: InetAddress,
+  @Column("src_port")
   source_port                 : Int,
-  source_ip_city              : String,
-  source_ip_country           : String,
-  source_ip_geohash           : String,
-  source_ip_geohash_value     : String,
-  source_ip_latitude          : String,
-  source_ip_longitude         : String,
+  source_ip_city              : Option[String],
+  source_ip_country           : Option[String],
+  source_ip_geohash           : Option[String],
+  source_ip_geohash_value     : Option[Int],
+  source_ip_latitude          : Option[Double],
+  source_ip_longitude         : Option[Double],
 ) extends Measurement {
   override def isLossy: Boolean = false
-
-  override val stream: Int = 0
 }
 
 object Flow extends MeasurementFactory {
