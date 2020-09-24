d7c4e249d0917a8304b176f1b92ecc41cde6da23
Daniel Oosterwijk <doosterw@waikato.ac.nz>
2020-09-25 09:34:05 +1200

    Add test for Akka wrappers.

---
+++ parameterTuner/src/test/scala/nz/net/wand/streamevmon/tuner/AkkaTest.scala
@@ -0,0 +1,80 @@
+package nz.net.wand.streamevmon.tuner
+
+import nz.net.wand.streamevmon.TestBase
+import nz.net.wand.streamevmon.tuner.jobs.{Job, JobResult, SimpleJob}
+import nz.net.wand.streamevmon.tuner.ActorManager.{JobResultHook, RemoveJobResultHook}
+
+import scala.concurrent.{Await, Future, TimeoutException}
+import scala.concurrent.duration.{Duration, SECONDS}
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class AkkaTest extends TestBase {
+  "Akka wrappers" should {
+    "execute code when submitted to" in {
+      var jobHasExecuted = false
+
+      ConfiguredPipelineRunner.submit(new Job("test-job") {
+        private val self: Job = this
+
+        override def run(): JobResult = {
+          jobHasExecuted = true
+          new JobResult {
+            override val job: Job = self
+          }
+        }
+      })
+
+      Thread.sleep(1000)
+      assert(jobHasExecuted)
+    }
+
+    "execute job result hooks" in {
+      var hookHasExecuted = false
+
+      ConfiguredPipelineRunner.addJobResultHook(
+        _ => hookHasExecuted = true
+      )
+
+      ConfiguredPipelineRunner.submit(SimpleJob("hello-world"))
+
+      // This will wait until hookHasExecuted goes true, or for 1 second, whichever comes first.
+      try {
+        Await.result(
+          Future(while (!hookHasExecuted) {}),
+          Duration(1, SECONDS)
+        )
+      }
+      catch {
+        case _: TimeoutException =>
+        case e => throw e
+      }
+
+      assert(hookHasExecuted)
+    }
+
+    "remove job result hooks" in {
+      var hookHasExecuted = false
+
+      val hook = new JobResultHook {
+        override def apply(jr: JobResult): Unit = hookHasExecuted = true
+      }
+
+      ConfiguredPipelineRunner.addJobResultHook(hook)
+      ConfiguredPipelineRunner.removeJobResultHook(RemoveJobResultHook(hook))
+
+      ConfiguredPipelineRunner.submit(SimpleJob("hello-world"))
+
+      try {
+        Await.result(
+          Future(while (!hookHasExecuted) {}),
+          Duration(1, SECONDS)
+        )
+      }
+      catch {
+        case _: TimeoutException =>
+        case e => throw e
+      }
+      assert(!hookHasExecuted)
+    }
+  }
+}
