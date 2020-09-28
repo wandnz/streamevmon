8f8be9b0b90cbbc97378f73ce63e0a113ce7c410
Daniel Oosterwijk <doosterw@waikato.ac.nz>
2020-09-28 14:22:01 +1300

    Add a little more documentation.

--- src/main/scala/nz/net/wand/streamevmon/parameters/HasParameterSpecs.scala
+++ src/main/scala/nz/net/wand/streamevmon/parameters/HasParameterSpecs.scala
@@ -57,6 +57,9 @@ trait HasParameterSpecs {
 }
 
 object HasParameterSpecs {
+  /** Maps DetectorType ValueBuilders onto the appropriate companion object that
+    * implements HasParameterSpecs.
+    */
   val supportedTypesMap: Map[DetectorType.ValueBuilder, HasParameterSpecs] = Map(
     DetectorType.Baseline -> BaselineDetector,
     DetectorType.Changepoint -> ChangepointDetector,
@@ -65,20 +68,32 @@ object HasParameterSpecs {
     DetectorType.Spike -> SpikeDetector,
   )
 
+  /** A list of supported companion objects that specify ParameterSpecs.
+    */
   val supportedTypes: Seq[HasParameterSpecs] = supportedTypesMap.values.toSeq
 
+  /** Gets all the ParameterSpecs for a particular DetectorType. */
   def parametersFromDetectorType(t: DetectorType.ValueBuilder): Iterable[ParameterSpec[Any]] =
     supportedTypesMap(t).parameterSpecs
 
+  /** Gets all the parameter constraints for a particular DetectorType. */
   def parameterRestrictionsFromDetectorType(t: DetectorType.ValueBuilder): Iterable[ComparableConstraint[Any]] =
     supportedTypesMap(t).parameterRestrictions
 
+  /** Gets all the parameters for all supported detectors. */
   val getAllDetectorParameters: Seq[ParameterSpec[Any]] =
     supportedTypes.flatMap(_.parameterSpecs)
 
+  /** Checks whether the given list of parameters is fully valid. If any
+    * detector finds a violation of one of its constraints, this function will
+    * return false.
+    */
   def parametersAreValid(params: Seq[ParameterInstance[Any]]): Boolean =
     supportedTypes.forall(_.parametersAreValid(params))
 
+  /** Certain parameters aren't useful to anyone but the end user. We provide
+    * default values for a number of them, for uses like parameter tuning.
+    */
   val fixedParameters: Map[String, Any] = Map(
     "detector.baseline.inactivityPurgeTime" -> Int.MaxValue,
     "detector.changepoint.inactivityPurgeTime" -> Int.MaxValue,
