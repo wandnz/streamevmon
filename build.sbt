9cee8abdd4c7fc005ed825c84fce0ec616acadf4
Daniel Oosterwijk <doosterw@waikato.ac.nz>
2021-01-21 15:18:48 +1300

    Update non-Flink dependencies that don't appear broken.

--- build.sbt
+++ build.sbt
@@ -3,7 +3,7 @@ ThisBuild / resolvers ++= Seq(
   Resolver.mavenLocal
 )
 
-ThisBuild / scalaVersion := "2.12.12"
+ThisBuild / scalaVersion := "2.12.13"
 
 import Dependencies._
 
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,20 +8,20 @@ object Dependencies {
   val flinkVersion = "1.11.2"
 
   // Used in multiple library declarations
-  val chroniclerVersion = "0.6.4"
+  val chroniclerVersion = "0.6.5"
   val retrofitVersion = "2.9.0"
   val scalaCacheVersion = "0.28.0"
 
   // Used in a single declaration each
   val slf4jVersion = "1.7.30"
-  val postgresqlVersion = "42.2.17"
-  val squerylVersion = "0.9.15"
-  val snakeyamlVersion = "2.1"
+  val postgresqlVersion = "42.2.18"
+  val squerylVersion = "0.9.16"
+  val snakeyamlVersion = "2.2.1"
   val jacksonVersion = "2.11.3"
   val jgraphtVersion = "1.5.0"
 
   // Used in tests - scalactic in coreDependencies shares a version with scalatest
-  val scalatestVersion = "3.2.2"
+  val scalatestVersion = "3.2.3"
   val testcontainersScalaVersion = "0.38.4"
 
   val providedDependencies: Seq[ModuleID] = Seq(
