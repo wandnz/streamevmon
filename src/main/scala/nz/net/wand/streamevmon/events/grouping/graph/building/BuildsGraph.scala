313dc647b2fadefcab7e3aa428c4c08bc94d9c3c
Daniel Oosterwijk <doosterw@waikato.ac.nz>
2021-04-21 10:26:40 +1200

    BuildsGraph supports CheckpointedFunction

--- src/main/scala/nz/net/wand/streamevmon/events/grouping/graph/building/BuildsGraph.scala
+++ src/main/scala/nz/net/wand/streamevmon/events/grouping/graph/building/BuildsGraph.scala
@@ -29,18 +29,51 @@ package nz.net.wand.streamevmon.events.grouping.graph.building
 import nz.net.wand.streamevmon.events.grouping.graph.GraphType._
 import nz.net.wand.streamevmon.events.grouping.graph.NoReflectionUnusableEdgeSupplier
 
+import org.apache.flink.api.common.state.{ListState, ListStateDescriptor}
+import org.apache.flink.runtime.state.{FunctionInitializationContext, FunctionSnapshotContext}
+
+/** Inherit this trait to properly receive [[GraphChangeEvent]]s. Whenever one
+  * is received, it should be passed to `receiveGraphChangeEvent`. The graph
+  * can be accessed using the `graph` field, which should not be altered
+  * manually in order to maintain consistency.
+  *
+  * If the inheriting class is a
+  * [[org.apache.flink.streaming.api.checkpoint.CheckpointedFunction CheckpointedFunction]],
+  * the `snapshotGraphState` and `initializeGraphState` functions should be
+  * called from the corresponding override functions.
+  */
 trait BuildsGraph {
   var graph: GraphT = _
 
-  def initialiseNewGraph(): Unit = {
+  private var graphState: ListState[GraphT] = _
+
+  def createNewGraph(): Unit = {
     graph = new GraphT(classOf[EdgeT])
     graph.setEdgeSupplier(new NoReflectionUnusableEdgeSupplier[EdgeT])
   }
 
   def receiveGraphChangeEvent(event: GraphChangeEvent): Unit = {
     if (graph == null) {
-      initialiseNewGraph()
+      createNewGraph()
     }
     event.apply(graph)
+  }
+
+  def snapshotGraphState(context    : FunctionSnapshotContext): Unit = {
+    graphState.clear()
+    graphState.add(graph)
+  }
+
+  def initializeGraphState(context    : FunctionInitializationContext): Unit = {
+    graphState = context
+      .getOperatorStateStore
+      .getUnionListState(new ListStateDescriptor("graph", classOf[GraphT]))
+
+    if (context.isRestored) {
+      graphState.get.forEach(entry => graph = entry)
+    }
+    else {
+      createNewGraph()
+    }
   }
 }
